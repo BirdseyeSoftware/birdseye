84f752036ef1ac1d5aa384ebd4bc7a029da0ee73
Tavis Rudd <tavis@damnsimple.com>
2012-01-09 20:18:00 -0800

    initial tests

--- test/birdseye/test/core.clj
+++ test/birdseye/test/core.clj
@@ -2,5 +2,40 @@
   (:use [birdseye.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(defn- assert-basic-site-map-props [sm]
+  (is (every? map? (vals sm)))
+  (is (every? keyword? (keys sm))))
+
+(deftest test-defsite-map
+  (let [sm (defsitemap
+             home
+             users
+             users.$userid
+             users.$userid.edit)]
+    (assert-basic-site-map-props sm)
+    (is (= 4 (-> sm keys count))))
+
+  (let [sm (defsitemap
+             home
+             foo
+             foo.bar
+             foo.bar.asdf
+             users nil                  ; nil allowed in place of {}
+             users.$userid {:2 (+ 1 1)}
+             users.$userid.edit {}
+             )]
+    (assert-basic-site-map-props sm)
+    (every? empty? (dissoc sm :users.$userid))
+    (is (= 2 (get-in sm [:users.$userid :2]))))
+
+  (is (thrown-with-msg? Exception #"Was expecting"
+        (defsitemap {} {}) ))
+
+  (is (thrown-with-msg? Exception #"Was expecting .* position 3"
+        (defsitemap foo bar {} {}) ))
+
+  (is (thrown-with-msg? Exception #"Was expecting .* position 2"
+        (defsitemap foo nil nil bar)))
+
+  (is (thrown-with-msg? Exception #"Parent node .* does not exist"
+        (defsitemap foo.bar) )))
