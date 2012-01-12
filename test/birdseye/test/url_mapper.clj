efe5e9587da562640c58035e43a8827fd3ed04fb
Tavis Rudd <tavis@damnsimple.com>
2012-01-12 09:33:45 -0800

    rm garbage

---
+++ test/birdseye/test/url_mapper.clj
@@ -0,0 +1,9 @@
+(ns birdseye.test.url-mapper
+  (:use [clojure.test])
+
+  (:require [clojure.pprint :as pp]
+            [clojure.repl :as repl])
+  (:use [birdseye.url-mapper]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
