2349b5fa179165f02360d1265730e7cc240fced7
Tavis Rudd <tavis@damnsimple.com>
2012-01-12 15:30:31 -0800

    rm old url-mapper test mod

--- test/birdseye/test/url_mapper.clj
+++
@@ -1,9 +0,0 @@
-(ns birdseye.test.url-mapper
-  (:use [clojure.test])
-
-  (:require [clojure.pprint :as pp]
-            [clojure.repl :as repl])
-  (:use [birdseye.url-mapper]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
