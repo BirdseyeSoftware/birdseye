e74ad951841f53f9c545eff4505f8210577cb16d
Tavis Rudd <tavis@damnsimple.com>
2012-01-12 11:01:20 -0800

    added another comment with interface ideas

--- src/birdseye/url_mapper.clj
+++ src/birdseye/url_mapper.clj
@@ -137,3 +137,49 @@
   ;;      [(node-to-url2 site-map k params)
   ;;       (node-key-to-routing-key k)])))
   )
+
+(comment
+  (defprotocol ApplicationMapper
+    ;; the router / middleware
+    (get-node-key-from-ring-req [this req])
+    (get-node-ctx-from-ring-req [this req])
+    (get-node-ctx [this node-key]))
+
+  (defprotocol NodeContext
+    ;; the user of these is the outermost app/controller layer
+    (get-controller [this]) ; could use compojure routing or other
+    (get-view [this req resp]) ; any function that transforms a resp
+
+    ;; the user of these is the 'view' or maybe the controller
+    (get-url [this & params])
+    (get-crumb [this])
+  ;;; get-params required/optional
+    )
+
+
+  (deftype SiteMap [sitemap]
+    ApplicationMapper
+                                        ; ... implement the stuff above
+    )
+
+  (deftype SiteNode [sitemap node-key]
+    NodeContext
+                                        ;...
+    )
+
+  (defprotocol RingAppMapper
+    (req-to-node-key [this req])
+    (req-to-node-ctx [this req])
+    (augment-ring-request [this req]))
+
+  (defn handle-request
+    "The outermost request handler below standard ring middleware"
+    [req]
+    (let [node-ctx          (req-to-node-ctx req)
+          req               (augment-ring-request node-ctx req)
+          controller        (get-controller node-ctx)
+          initial-resp      (controller req)
+          view              (get-view node-ctx req initial-resp)
+          final-resp        (view req initial-resp)]
+      final-resp))
+  )
