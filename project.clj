db1b1bf205192365abc4ccafd13ce2e12f47fbe1
Tavis Rudd <tavis@damnsimple.com>
2012-01-12 09:44:27 -0800

    add ring ring-mock deps

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "A sitemap/url-routing system for Clojure web apps."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/core.match "0.2.0-alpha8"]
-                 ])
+                 [ring "1.0.1"]]
+  :dev-dependencies [[ring-mock "0.1.1"]])
