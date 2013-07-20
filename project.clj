4e7e1eac4c96fb0aa8c493a1c218ead2202ff98b
Tavis Rudd <tavis@damnsimple.com>
2013-07-19 17:21:39 -0700

    add missing :license and :url on project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
-(defproject com.birdseye-sw/birdseye "1.0.4"
+(defproject com.birdseye-sw/birdseye "1.0.5"
   :description "A sitemap/url-routing system for Clojure/ring web apps."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.match "0.2.0-rc4"]
                  [ring "1.2.0"]
                  [clout "1.1.0"]]
   :exclusions [clj-stacktrace]
+  :url "https://github.com/BirdseyeSoftware/birdseye"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
   :dev-dependencies [[ring-mock "0.1.5"]])
